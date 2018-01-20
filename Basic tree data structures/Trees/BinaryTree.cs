4407d975780d311109bd341a1fc6ae2955044472
Ulian Ashikov <u.ashikov@abv.bg>
2018-01-20 22:39:38 +0200

    Basic tree data structures lab

---
+++ Basic tree data structures/Trees/BinaryTree.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+public class BinaryTree<T>
+{
+    public T Value { get; private set; }
+
+    public BinaryTree<T> Left { get; private set; }
+
+    public BinaryTree<T> Right { get; private set; }
+
+    public BinaryTree(T value, BinaryTree<T> leftChild = null, BinaryTree<T> rightChild = null)
+    {
+        this.Value = value;
+        this.Left = leftChild;
+        this.Right = rightChild;
+    }
+
+    public void PrintIndentedPreOrder(int indent = 0)
+    {
+        Console.WriteLine($"{new string(' ', 2 * indent)}{this.Value}");
+
+        if (this.Left != null)
+        {
+            this.Left.PrintIndentedPreOrder(indent + 1);
+        }
+
+        if (this.Right != null)
+        {
+            this.Right.PrintIndentedPreOrder(indent + 1);
+        }
+    }
+
+    public void EachInOrder(Action<T> action)
+    {
+        if (this.Left != null)
+        {
+            this.Left.EachInOrder(action);
+        }
+
+        action.Invoke(this.Value);
+
+        if (this.Right != null)
+        {
+            this.Right.EachInOrder(action);
+        }
+    }
+
+    public void EachPostOrder(Action<T> action)
+    {
+        if (this.Left != null)
+        {
+            this.Left.EachPostOrder(action);
+        }
+
+        if (this.Right != null)
+        {
+            this.Right.EachPostOrder(action);
+        }
+
+        action.Invoke(this.Value);
+    }
+}
---
+++ Basic tree data structures/Trees/Tree.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Tree<T>
+{
+    public T Value { get; private set; }
+
+    public Tree<T>[] Children { get; private set; }
+
+    public Tree(T value, params Tree<T>[] children)
+    {
+        this.Value = value;
+        this.Children = children;
+    }
+
+    public void Print(int indent = 0)
+    {
+        this.PrintRecursively(this,indent);
+    }
+
+    private void PrintRecursively(Tree<T> node, int indent)
+    {
+        Console.WriteLine($"{new string(' ',indent)}{node.Value}");
+
+        foreach (Tree<T> child in node.Children)
+        {
+            this.PrintRecursively(child, indent+2);
+        }
+    }
+
+    public void Each(Action<T> action)
+    {
+        this.EachRecursively(this, action);
+    }
+
+    private void EachRecursively(Tree<T> node, Action<T> action)
+    {
+        action.Invoke(node.Value);
+
+        foreach (var child in node.Children)
+        {
+            this.EachRecursively(child, action);
+        }
+    }
+
+    public IEnumerable<T> OrderDFS()
+    {
+        var result = new List<T>();
+
+        this.OrderDFSRecursively(this, result);
+
+        return result;
+    }
+
+    private void OrderDFSRecursively(Tree<T> node, List<T> result)
+    {
+        foreach (var child in node.Children)
+        {
+            this.OrderDFSRecursively(child, result);
+        }
+
+        result.Add(node.Value);
+    }
+
+    public IEnumerable<T> OrderBFS()
+    {
+        var queue = new Queue<Tree<T>>();
+        queue.Enqueue(this);
+
+        var result = new List<T>();
+
+        while (queue.Count > 0)
+        {
+            var node = queue.Dequeue();
+            result.Add(node.Value);
+
+            foreach (var child in node.Children)
+            {
+                queue.Enqueue(child);
+            }
+        }
+
+        return result;
+    }
+}
