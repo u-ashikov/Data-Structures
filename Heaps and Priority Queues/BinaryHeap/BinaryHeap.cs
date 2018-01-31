7dbbdb761d5b76c38dd37e8694d83852ffcf2782
Ulian Ashikov <u.ashikov@abv.bg>
2018-01-31 18:25:34 +0200

    Heaps and priority queues lab

---
+++ Heaps and Priority Queues/BinaryHeap/BinaryHeap.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class BinaryHeap<T> where T : IComparable<T>
+{
+    private List<T> heap;
+
+    public BinaryHeap()
+    {
+        this.heap = new List<T>();
+    }
+
+    public int Count
+    {
+        get => this.heap.Count;
+    }
+
+    public void Insert(T item)
+    {
+        this.heap.Add(item);
+
+        int itemIndex = this.Count - 1;
+        int parentIndex = (itemIndex - 1) / 2;
+
+        this.HeapifyUp(itemIndex, parentIndex);
+    }
+
+    public T Peek()
+    {
+        if (this.Count == 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        return this.heap[0];
+    }
+
+    public T Pull()
+    {
+        if (this.Count == 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        var removedElement = this.heap[0];
+
+        this.Swap(0, this.Count - 1);
+        this.heap.RemoveAt(this.Count - 1);
+
+        this.HeapifyDown(0);
+
+        return removedElement;
+    }
+
+    private void HeapifyUp(int childIndex, int parentIndex)
+    {
+        if (parentIndex < 0)
+        {
+            return;
+        }
+
+        var element = this.heap[childIndex];
+        var parent = this.heap[parentIndex];
+
+        if (element.CompareTo(parent) > 0)
+        {
+            this.Swap(childIndex, parentIndex);
+            this.HeapifyUp(parentIndex, parentIndex - 1);
+        }
+    }
+
+    private void HeapifyDown(int parentIndex)
+    {
+        if (parentIndex >= this.Count / 2)
+        {
+            return;
+        }
+
+        int childIndex = (2 * parentIndex) + 1;
+        int rightChildIndex = childIndex + 1;
+        var leftChild = this.heap[childIndex];
+
+        if (rightChildIndex < this.Count && this.heap[rightChildIndex].CompareTo(leftChild) > 0)
+        {
+            childIndex++;
+        }
+
+        int compare = this.heap[parentIndex].CompareTo(this.heap[childIndex]);
+
+        if (compare < 0)
+        {
+            this.Swap(childIndex, parentIndex);
+            this.HeapifyDown(parentIndex++);
+        }
+    }
+
+    private void Swap(int childIndex, int parentIndex)
+    {
+        var oldParent = this.heap[parentIndex];
+        this.heap[parentIndex] = this.heap[childIndex];
+        this.heap[childIndex] = oldParent;
+    }
+}
---
+++ Heaps and Priority Queues/BinaryHeap/Heap.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+public static class Heap<T> where T : IComparable<T>
+{
+    public static void Sort(T[] arr)
+    {
+        ConstructHeap(arr);
+        SortHeap(arr);
+    }
+
+    private static void SortHeap(T[] arr)
+    {
+        for (int i = arr.Length - 1; i >= 0; i--)
+        {
+            Swap(0, i, arr);
+            HeapifyDown(0, i, arr);
+        }
+    }
+
+    private static void ConstructHeap(T[] arr)
+    {
+        HeapifyDown(0, arr.Length, arr);
+    }
+
+    private static void HeapifyDown(int parentIndex, int length, T[] heap)
+    {
+        if (parentIndex >= length / 2)
+        {
+            return;
+        }
+
+        int childIndex = (2 * parentIndex) + 1;
+        int rightChildIndex = childIndex + 1;
+        var leftChild = heap[childIndex];
+
+        if (rightChildIndex < length && heap[rightChildIndex].CompareTo(leftChild) > 0)
+        {
+            childIndex++;
+        }
+
+        int compare = heap[parentIndex].CompareTo(heap[childIndex]);
+
+        if (compare < 0)
+        {
+            Swap(childIndex, parentIndex, heap);
+        }
+
+        parentIndex++;
+
+        HeapifyDown(parentIndex, length, heap);
+    }
+
+    private static void Swap(int childIndex, int parentIndex, T[] heap)
+    {
+        var oldParent = heap[parentIndex];
+        heap[parentIndex] = heap[childIndex];
+        heap[childIndex] = oldParent;
+    }
+}
